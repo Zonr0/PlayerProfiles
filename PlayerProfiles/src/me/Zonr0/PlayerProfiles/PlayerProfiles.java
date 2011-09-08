f731dd1a2e29c00ff591eedf9fb1e8344a5cc353
Zonr_0 <ZonrZero@gmail.com>
2011-09-07 23:43:09 -0700

    Added initial database work. Now uses PatPeter's SQLibrary. Database is created along with a basic table.

--- PlayerProfiles/src/me/Zonr0/PlayerProfiles/PlayerProfiles.java
+++ PlayerProfiles/src/me/Zonr0/PlayerProfiles/PlayerProfiles.java
@@ -3,12 +3,18 @@ package me.Zonr0.PlayerProfiles;
 import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 import java.util.logging.Logger;
+import lib.PatPeter.SQLibrary.*;
 
 //comment
 
 public class PlayerProfiles extends JavaPlugin {
 
+	public SQLite dbManager;
+	public String logPrefix = "[PlayerProfiles]";
+	public File pFolder = new File("plugins/PlayerProfiles");
 	
 	Logger log = Logger.getLogger("Minecraft");
 	
@@ -17,12 +23,36 @@ public class PlayerProfiles extends JavaPlugin {
 	public void onEnable() {
 		log.info("PlayerProfiles has been enabled!");
 		
+		createPluginFolder();
+		
+		String TableCreationQuery = "CREATE TABLE profiles ( 'id' INTEGER PRIMARY KEY, 'username' VARCHAR(30) NOT NULL, 'realname' VARCHAR(80), twitteraccount' VARCHAR(30), 'from' VARCHAR(255), 'firstregistered' VARCHAR(255) NOT NULL, 'lastSeen' VARCHAR(255));";
+		
+		dbManager = new SQLite(this.log, this.logPrefix, "profiles", pFolder.getPath());
+		dbManager.open();
+		
+		if(dbManager.createTable(TableCreationQuery))
+		{
+			log.info("Table creation succesful");
+		}
+		else
+		{
+			log.info("Table failed to initialize");
+		}
+		
 		PluginManager pm = this.getServer().getPluginManager();
 		pm.registerEvent(Event.Type.PLAYER_COMMAND_PREPROCESS, playerListener, Event.Priority.Normal, this);
 	}
 	
 	public void onDisable() {
+		if (dbManager != null) {
+			dbManager.close();
+		}
 		log.info("PlayerProfiles has been disabled.");
 	}
 	
+	public void createPluginFolder() {
+		if (!this.pFolder.exists()) {
+			pFolder.mkdir();
+		}
+	}
 }
--- PlayerProfiles/src/me/Zonr0/PlayerProfiles/PlayerProfilesPlayerListener.java
+++ PlayerProfiles/src/me/Zonr0/PlayerProfiles/PlayerProfilesPlayerListener.java
@@ -1,19 +1,25 @@
 package me.Zonr0.PlayerProfiles;
 
+import java.util.logging.Logger;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.player.PlayerListener;
 
+import lib.PatPeter.SQLibrary.*;
+
 public class PlayerProfilesPlayerListener extends PlayerListener {
 	
 	public static PlayerProfiles plugin;
+	private Logger log = Logger.getLogger("Minecraft");
+	
 	public PlayerProfilesPlayerListener(PlayerProfiles instance) {
 		plugin = instance;
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
 		if(cmd.getName().equalsIgnoreCase("debug")){ // If the player typed /basic then do the following...
-			
+			log.info("Debug information goes here.");
 			return true;
 		} //If this has happened the function will break and return true. if this hasn't happened the a value of false will be returned.
 		return false; 
